95cffcf042bf3f779ffa916e573bd117f3ac5423
Martin Schulze <schulze@fzi.de>
2016-09-22 19:24:53 +0200

    NEST release 2.10.0

---
+++ libnestutil/config.h
@@ -0,0 +1,355 @@
+/* libnestutil/config.h.  Generated from config.h.in by configure.  */
+/* libnestutil/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* tics per ms in simulation */
+/* #undef CONFIG_TICS_PER_MS */
+
+/* tics per step in simulation */
+/* #undef CONFIG_TICS_PER_STEP */
+
+/* define if the compiler does not include *.h headers ISO conformant */
+/* #undef HAVE_ALPHA_CXX_STD_BUG */
+
+/* Define to 1 if you have the `argz_add' function. */
+#define HAVE_ARGZ_ADD 1
+
+/* Define to 1 if you have the `argz_append' function. */
+#define HAVE_ARGZ_APPEND 1
+
+/* Define to 1 if you have the `argz_count' function. */
+#define HAVE_ARGZ_COUNT 1
+
+/* Define to 1 if you have the `argz_create_sep' function. */
+#define HAVE_ARGZ_CREATE_SEP 1
+
+/* Define to 1 if you have the <argz.h> header file. */
+#define HAVE_ARGZ_H 1
+
+/* Define to 1 if you have the `argz_insert' function. */
+#define HAVE_ARGZ_INSERT 1
+
+/* Define to 1 if you have the `argz_next' function. */
+#define HAVE_ARGZ_NEXT 1
+
+/* Define to 1 if you have the `argz_stringify' function. */
+#define HAVE_ARGZ_STRINGIFY 1
+
+/* Define to 1 if you have the `closedir' function. */
+#define HAVE_CLOSEDIR 1
+
+/* define if the compiler ignores cmath makros */
+/* #undef HAVE_CMATH_MAKROS_IGNORED */
+
+/* Define to 1 if you have the declaration of `cygwin_conv_path', and to 0 if
+   you don't. */
+/* #undef HAVE_DECL_CYGWIN_CONV_PATH */
+
+/* Define to 1 if you have the <dirent.h> header file. */
+#define HAVE_DIRENT_H 1
+
+/* Define if you have the GNU dld library. */
+/* #undef HAVE_DLD */
+
+/* Define to 1 if you have the <dld.h> header file. */
+/* #undef HAVE_DLD_H */
+
+/* Define to 1 if you have the `dlerror' function. */
+#define HAVE_DLERROR 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <dl.h> header file. */
+/* #undef HAVE_DL_H */
+
+/* Define if you have the _dyld_func_lookup function. */
+/* #undef HAVE_DYLD */
+
+/* Define to 1 if the system has the type `error_t'. */
+#define HAVE_ERROR_T 1
+
+/* Define to 1 if you have the `execv' function. */
+#define HAVE_EXECV 1
+
+/* "Define if expm1() is available" */
+#define HAVE_EXPM1 1
+
+/* Define to 1 if you have the `fork' function. */
+#define HAVE_FORK 1
+
+/* Is the GNU Science Library available (ver. >= 1.0)? */
+#define HAVE_GSL 1
+
+/* Is GSL Version >= 1.11? */
+#define HAVE_GSL_1_11 1
+
+/* Is GSL Version >= 1.2? */
+#define HAVE_GSL_1_2 1
+
+/* Do we have the inline macro in C? */
+#define HAVE_INLINE 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* "Define if isnan() is available" */
+#define HAVE_ISNAN 1
+
+/* define for istream */
+#define HAVE_ISTREAM /**/
+
+/* Define if you have the libdl library or equivalent. */
+#define HAVE_LIBDL 1
+
+/* Define if libdlloader will be built on this platform */
+#define HAVE_LIBDLLOADER 1
+
+/* Havel libltdl, can load dynamic modules */
+#define HAVE_LIBLTDL 1
+
+/* libneurosim support enabled? */
+/* #undef HAVE_LIBNEUROSIM */
+
+/* define if we have usable long long type. */
+#define HAVE_LONG_LONG 1
+
+/* Define this if a modern libltdl is already installed */
+#define HAVE_LTDL 1
+
+/* Define to 1 if you have the <mach/mach.h> header file. */
+/* #undef HAVE_MACH_MACH_H */
+
+/* Define to 1 if you have the <mach-o/dyld.h> header file. */
+/* #undef HAVE_MACH_O_DYLD_H */
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* MPI is available. */
+/* #undef HAVE_MPI */
+
+/* MUSIC support enabled? */
+/* #undef HAVE_MUSIC */
+
+/* define if M_E is defined */
+#define HAVE_M_E /**/
+
+/* define if M_PI is defined */
+#define HAVE_M_PI /**/
+
+/* "Define if NAN is available" */
+#define HAVE_NAN 1
+
+/* Define to 1 if you have the `opendir' function. */
+#define HAVE_OPENDIR 1
+
+/* define for ostream */
+#define HAVE_OSTREAM /**/
+
+/* Define to 1 if you have the `pclose' function. */
+#define HAVE_PCLOSE 1
+
+/* Define to 1 if you have the `popen' function. */
+#define HAVE_POPEN 1
+
+/* Define if libtool can extract symbol lists from object files. */
+#define HAVE_PRELOADED_SYMBOLS 1
+
+/* Define to 1 if you have the `readdir' function. */
+#define HAVE_READDIR 1
+
+/* Use GNU libreadline */
+#define HAVE_READLINE 1
+
+/* Define to 1 if you have the `setenv' function. */
+#define HAVE_SETENV 1
+
+/* Define if you have the shl_load function. */
+/* #undef HAVE_SHL_LOAD */
+
+/* Define to 1 if you have the `sigaction' function. */
+#define HAVE_SIGACTION 1
+
+/* define if the compiler ignores symbolic signal names in signal.h */
+/* #undef HAVE_SIGUSR_IGNORED */
+
+/* define for sstream */
+#define HAVE_SSTREAM /**/
+
+/* define if the compiler fails on static template member declaration */
+/* #undef HAVE_STATIC_TEMPLATE_DECLARATION_FAILS */
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* "Define if std::isnan() is available" */
+#define HAVE_STD_ISNAN 1
+
+/* "Define if std::nan(NULL) is available" */
+/* #undef HAVE_STD_NAN */
+
+/* define for STL vector capacity base unity */
+#define HAVE_STL_VECTOR_CAPACITY_BASE_UNITY /**/
+
+/* define for STL vector capacity doubling strategy */
+#define HAVE_STL_VECTOR_CAPACITY_DOUBLING /**/
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strlcat' function. */
+/* #undef HAVE_STRLCAT */
+
+/* Define to 1 if you have the `strlcpy' function. */
+/* #undef HAVE_STRLCPY */
+
+/* Define to 1 if you have the <sys/dl.h> header file. */
+/* #undef HAVE_SYS_DL_H */
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Tics per millisecond specified? */
+/* #undef HAVE_TICS_PER_MS */
+
+/* Tics per step specified? */
+/* #undef HAVE_TICS_PER_STEP */
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Vampir support enabled? */
+/* #undef HAVE_VAMPIR */
+
+/* This value is set to 1 to indicate that the system argz facility works */
+#define HAVE_WORKING_ARGZ 1
+
+/* define if the compiler fails with ICE */
+/* #undef HAVE_XLC_ICE_ON_USING */
+
+/* Configuring for Blue Gene */
+/* #undef IS_BLUEGENE */
+
+/* Configuring for Blue Gene/L */
+/* #undef IS_BLUEGENE_L */
+
+/* Configuring for Blue Gene/P */
+/* #undef IS_BLUEGENE_P */
+
+/* Configuring for Blue Gene/Q */
+/* #undef IS_BLUEGENE_Q */
+
+/* Configuring for K */
+/* #undef IS_K */
+
+/* Define if the OS needs help to load dependent libraries for dlopen(). */
+/* #undef LTDL_DLOPEN_DEPLIBS */
+
+/* Define to the system default library search path. */
+#define LT_DLSEARCH_PATH "/lib:/usr/lib:/usr/lib/x86_64-linux-gnu/libfakeroot:/usr/local/lib:/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu:/usr/lib/x86_64-linux-gnu/mesa-egl:/usr/lib/x86_64-linux-gnu/mesa"
+
+/* The archive extension */
+#define LT_LIBEXT "a"
+
+/* The archive prefix */
+#define LT_LIBPREFIX "lib"
+
+/* Define to the extension used for runtime loadable modules, say, ".so". */
+#define LT_MODULE_EXT ".so"
+
+/* Define to the name of the environment variable that determines the run-time
+   module search path. */
+#define LT_MODULE_PATH_VAR "LD_LIBRARY_PATH"
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to the shared library suffix, say, ".dylib". */
+/* #undef LT_SHARED_EXT */
+
+/* Define if dlsym() requires a leading underscore in symbol names. */
+/* #undef NEED_USCORE */
+
+/* Name of package */
+#define PACKAGE "nest"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "nest_user@nest-initiative.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "nest"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "nest 2.10.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "nest"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "2.10.0"
+
+/* Use PS array construction semantics */
+#define PS_ARRAYS 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Enable extensions on AIX 3, Interix.  */
+#ifndef _ALL_SOURCE
+# define _ALL_SOURCE 1
+#endif
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+/* Enable threading extensions on Solaris.  */
+#ifndef _POSIX_PTHREAD_SEMANTICS
+# define _POSIX_PTHREAD_SEMANTICS 1
+#endif
+/* Enable extensions on HP NonStop.  */
+#ifndef _TANDEM_SOURCE
+# define _TANDEM_SOURCE 1
+#endif
+/* Enable general extensions on Solaris.  */
+#ifndef __EXTENSIONS__
+# define __EXTENSIONS__ 1
+#endif
+
+
+/* Version number of package */
+#define VERSION "2.10.0"
+
+/* Define to 1 if on MINIX. */
+/* #undef _MINIX */
+
+/* Define to 2 if the system does not provide POSIX.1 features except with
+   this defined. */
+/* #undef _POSIX_1_SOURCE */
+
+/* Define to 1 if you need to in order for `stat' and other things to work. */
+/* #undef _POSIX_SOURCE */
+
+/* Define so that glibc/gnulib argp.h does not typedef error_t. */
+/* #undef __error_t_defined */
+
+/* Define to a type to use for `error_t' if it is not otherwise available. */
+/* #undef error_t */
+
+/* Define as `__inline' or `__inline__' if that's what the
+    C compiler calls it or to nothing if it is not supported. */
+#ifndef __cplusplus
+/* # undef inline */
+#endif
---
+++ libnestutil/sliconfig.h
@@ -0,0 +1,35 @@
+#ifndef SLICONFIG_H
+#define SLICONFIG_H
+/* 
+    Configuration header.
+    sliconfig.h is automatically generated from sliconfig.h.in
+    during the configuration process. 
+
+    Please make all changes in sliconfig.h.in.
+*/
+
+#include "config.h"
+
+#define SLI_PRGNAME "nest-2.10.0"
+
+#define SLI_MAJOR_REVISION 2
+#define SLI_MINOR_REVISION 10
+#define SLI_PATCHLEVEL "0"
+
+#define SLI_HOST "x86_64-unknown-linux-gnu"
+#define SLI_HOSTOS "linux-gnu"
+#define SLI_HOSTCPU "x86_64"
+#define SLI_HOSTVENDOR "unknown"
+
+#define SLI_SOURCEDIR "/home/perser/workspace/nest-2.10.0"
+#define SLI_BUILDDIR "/home/perser/workspace/nest-2.10.0"
+#define SLI_PREFIX "/home/perser/workspace/nest-2.10.0/debug_stuff"
+
+#define SLI_EXITCODE_ABORT 134
+#define SLI_EXITCODE_SEGFAULT 139
+
+/* Do not delete the empty line above ! Configure will not work correctly
+   on some systems without it!
+*/
+#endif
+/** end of file **/
